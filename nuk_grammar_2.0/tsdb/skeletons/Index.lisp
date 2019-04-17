97e95baf2c63ed9dd2bbfc47f3aacebff124de67
UbuntuLKB User <cmdowney88@gmail.com>
2019-04-17 11:24:58 -0700

    Remove conflicting person constraints from verb-pc2 and verb-pc53

--- nuk_grammar_2.0/tsdb/skeletons/Index.lisp
+++ nuk_grammar_2.0/tsdb/skeletons/Index.lisp
@@ -23,4 +23,7 @@
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ; New test suites here. For example:
 ; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
+((:path . "corpus") (:content . "Original IGT from pipeline"))
+((:path . "corpus_no_punc") (:content . "IGT with sentential punctuation removed"))
 )
+
