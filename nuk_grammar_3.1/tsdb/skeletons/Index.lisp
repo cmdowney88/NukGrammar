f98128cd9549a1a372b9efb09d8bb944e8398495
UbuntuLKB User <cmdowney88@gmail.com>
2019-04-25 15:52:02 -0700

    Add more general testsuite skeleton description

--- nuk_grammar_3.1/tsdb/skeletons/Index.lisp
+++ nuk_grammar_3.1/tsdb/skeletons/Index.lisp
@@ -25,7 +25,6 @@
 ; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
 ((:path . "corpus") (:content . "Original IGT from pipeline"))
 ((:path . "corpus_no_punc") (:content . "IGT with sentential punctuation removed"))
-((:path . "testsuite") (:content . "Constructed Testsuite for Lab 3"))
-((:path . "lab4_testsuite") (:content . "Constructed Testsuite for Lab 4"))
+((:path . "testsuite") (:content . "Constructed Testsuite"))
 )
 
